535abcb43b7d823ab549ff02b1da998d03f158f0
mttmortensen <mtt.mortensen527@gmail.com>
2024-11-30 21:28:36 -0700

    getting out of index range error. Wonder if this is different for Lists

--- ScratchPad/gptDSA_112924.cs
+++ ScratchPad/gptDSA_112924.cs
@@ -14,13 +14,13 @@ namespace ScratchPad
 
             List<string> usernames = new List<string>();
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i <= 5; i++)
             {
                 Console.WriteLine($"Name {i + 1}:");
                 usernames[i] = Console.ReadLine();
             }
 
-            for (int i = 0; i < usernames.Count; i++) 
+            for (int i = 0; i <= usernames.Count; i++) 
             {
                 Console.WriteLine($"Here you go!:");
                 Console.Write($"{usernames[i]}, ");
