e831d1c42d08a97fa96b1630a5429278d891a1a5
mttmortensen <mtt.mortensen527@gmail.com>
2024-12-15 10:11:25 -0700

    wrote out list solution for even numbers.

--- ScratchPad/gptLISTS_121524.cs
+++ ScratchPad/gptLISTS_121524.cs
@@ -10,7 +10,31 @@ namespace ScratchPad
     {
         public static void FilterEvenNumbers()
         {
+            List<int> listOfNumbers = new List<int>();
 
+            List<int> evenNumbers = new List<int>();
+
+            for (int i = 0; i < listOfNumbers.Count; i++) 
+            {
+                listOfNumbers.Add(Convert.ToInt32(Console.ReadLine()));
+
+                if (listOfNumbers[i] % 2 == 0)
+                {
+                    evenNumbers.Add(listOfNumbers[i]);
+                }
+            }
+
+            Console.WriteLine("The even numbers in your list are: ");
+            foreach (int i in evenNumbers) 
+            {
+                Console.Write($"{i}, ");
+
+                // Handling last entry
+                if (evenNumbers.Count - 1 == -1 )
+                {
+                    Console.Write($"{i}");
+                }
+            }
         }
     }
 }
