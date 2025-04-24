b82ee8e26ea55cc03ece4fb1973f3c4474c17bb0
mttmortensen <mtt.mortensen527@gmail.com>
2025-04-23 21:07:56 -0600

    variable and output text setup

--- ScratchPad/gptLISTS_042325.cs
+++ ScratchPad/gptLISTS_042325.cs
@@ -8,6 +8,18 @@ namespace ScratchPad
 {
     public class gptLISTS_042325
     {
+        public static void DoubleEvensRemoveOdds()
+        {
+            List<int> listOfNumbers = new List<int>();
 
+            List<int> doubledEvenNumbers = new List<int>();
+
+            Console.WriteLine("Enter 5 random numbers and I'll double the even and remove the odd ones.");
+
+            for (int i = 0; i < 5; i++)
+            {
+
+            }
+        }
     }
 }
