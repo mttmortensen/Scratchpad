db1464725ae2b77714bbfd5cacbc9f742b3d07ce
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-07 16:15:27 -0600

    using instances to test methods since dealing with OOP

--- ScratchPad/Program.cs
+++ ScratchPad/Program.cs
@@ -4,7 +4,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        gptLISTS_042625.GroceryInventoryCatagoryManager();
+        // Create an instance of the gptOOP_050725 class
+        gptOOP_050725 gptOOP_050725 = new gptOOP_050725();
+
+        // Call the TestMethod to execute the code
+        gptOOP_050725.TestMethod();
 
         Console.ReadLine();
     }
