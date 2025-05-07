f3f2abdff402da9e069d2a32ae8c897e054e0d6e
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-07 16:15:14 -0600

    added User as internal so that I can follow the same gpt Class structure

--- ScratchPad/gptOOP_050725.cs
+++ ScratchPad/gptOOP_050725.cs
@@ -8,6 +8,30 @@ namespace ScratchPad
 {
     public class gptOOP_050725
     {
+        
 
+        public void TestMethod()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+
+            User user = new User(userName);
+            Console.WriteLine(user.Greet());
+        }
+    }
+
+    internal class User
+    {
+        private string _name;
+
+        public User(string name)
+        {
+            _name = name;
+        }
+
+        public string Greet()
+        {
+            return $"Hello {_name}!";
+        }
     }
 }
