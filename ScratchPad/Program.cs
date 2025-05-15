9cacc9b6eb69af82338a5d7d7a6cb06ef6788c00
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-15 09:46:51 -0600

    Trying out the challenge with the first part

--- ScratchPad/Program.cs
+++ ScratchPad/Program.cs
@@ -5,27 +5,34 @@ internal class Program
     private static void Main(string[] args)
     {
         // Challenge 2
-        List<Character> party = new List<Character> 
-        {
-            new Warrior("Throg"),
-            new Mage("Elara"),
-            new Thief("Lirael")
-        };
+        //List<Character> party = new List<Character> 
+        //{
+        //    new Warrior("Throg"),
+        //    new Mage("Elara"),
+        //    new Thief("Lirael")
+        //};
 
-        foreach (var character in party)
-        {
-            Console.WriteLine(character.Attack());
-        }
+        //foreach (var character in party)
+        //{
+        //    Console.WriteLine(character.Attack());
+        //}
 
         // Challenge 3
-        Mage mage = new Mage("Elara");
-        Console.WriteLine(mage.Attack());
-        Console.WriteLine(mage.TakeDamage(20));
-        Console.WriteLine(mage.GetHealth());
-        Console.WriteLine(mage.Heal(30));
+        //Mage mage = new Mage("Elara");
+        //Console.WriteLine(mage.Attack());
+        //Console.WriteLine(mage.TakeDamage(20));
+        //Console.WriteLine(mage.GetHealth());
+        //Console.WriteLine(mage.Heal(30));
         // This should come back with the same value, 80, since the max health is 100  and last statement was 80 + 30
         // It's a bug but it still works
-        Console.WriteLine(mage.GetHealth()); 
+        //Console.WriteLine(mage.GetHealth()); 
+
+        // Challenge 4A
+        Warrior warrior = new Warrior("Throg");
+        Console.WriteLine(warrior.TakeDamage(30));
+        Console.WriteLine(warrior.GetHealth());
+        Console.WriteLine(warrior.Rest());
+        Console.WriteLine(warrior.GetHealth());
 
         Console.ReadLine();
     }
