3893b34012e5a756cde43c3b80a1a3270b5bdc46
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-26 10:02:57 -0600

    starting up the first challenge from chat on LC lists and arrays

---
+++ Leetcode/gpt_ListsArrays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class gpt_ListsArrays
+    {
+        public int TakeAListAndReverseItToAString(List<int> list) 
+        {
+            return 
+        }
+    }
+}
