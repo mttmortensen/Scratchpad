fde9e7a37d0b742c96206681baf577c62dfd6ea3
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-28 22:00:25 -0600

    challenge from chat to repersent an eaiser version of a easy LC challenge

--- Leetcode/gpt_ListsArrays.cs
+++ Leetcode/gpt_ListsArrays.cs
@@ -8,11 +8,23 @@ namespace Leetcode
 {
     public class gpt_ListsArrays
     {
-        public string TakeAListAndReverseItToAString(List<int> list) 
+        public string TakeAListAndReverseItToAString(List<int> listOfInts) 
         {
             // Need to covert the list of ints to list of strings first
+            List<string> listOfStrings = new List<string>();
+
+            foreach (int number in listOfInts) 
+            {
+                listOfStrings.Add(number.ToString());
+            }
+
             // Somehow then reverse the list of strings 
+            listOfStrings.Reverse();
+
             // And then turn it into a single string
+            String singleString = string.Join(", ", listOfStrings);
+
+            return singleString;
         }
     }
 }
