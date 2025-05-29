ecc722e8853fe2b91696eba7495eefe446c08b2b
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-29 09:02:40 -0600

    updated input logic to be its own function in gptLists_052925

--- ChatChallenges/Program.cs
+++ ChatChallenges/Program.cs
@@ -4,46 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Enter a list of numbers and I'll return only the even ones");
-        string stringOfNumbers = Console.ReadLine();
-
-        // I need to split up the string into a list now
-        string[] stringNums = stringOfNumbers.Split();
-
-        // The return value of listOfNumbers is an array of strings.
-
-        // I'll get a new List ready to hold the converted nums
-        List<int> listOfNumbers = new List<int>();
-
-        // Then I'll convert it to the int type
-        foreach (string strNum in stringNums)
-        { 
-            // The conversion
-            int.TryParse(strNum, out int num);
-            // Adding to the new even list
-            listOfNumbers.Add(num);
-        }
-
-        // Pass it through to the function with some error handling
-        // This is kinda bad practice as we should do the error hanlding in the function itself 
-        // But this is just for understanding and doing it ourselves.
-        List<int> evens = gptLISTS_052925.OnlyReturningEvenNumbers(listOfNumbers);
-
-        // This is a List method to check to see if the Evens List has any elements
-        if (!evens.Any())
-        {
-            Console.WriteLine("Doesn't look like there was any evens!");
-        }
-        else
-        {
-            Console.WriteLine("\nEven ones are: ");
-            foreach (int e in evens)
-            {
-
-                Console.WriteLine($"{e}");
-            }
-        }
-
+        gptLISTS_052925.GatherListInputsAndReturnEvens();
         Console.ReadLine();
     }
 }
--- ChatChallenges/gptLISTS_052925.cs
+++ ChatChallenges/gptLISTS_052925.cs
@@ -8,7 +8,51 @@ namespace ChatChallenges
 {
     public static class gptLISTS_052925
     {
-        public static List<int> OnlyReturningEvenNumbers(List<int> listOfNumbers)         
+
+        // This function is for gathering inputs with some error handling
+        // It will call the logic function below which is set to private
+        public static void GatherListInputsAndReturnEvens() 
+        {
+            Console.WriteLine("Enter a list of numbers and I'll return only the even ones");
+            string stringOfNumbers = Console.ReadLine();
+
+            // I need to split up the string into a list now
+            string[] stringNums = stringOfNumbers.Split();
+
+            // The return value of listOfNumbers is an array of strings.
+
+            // I'll get a new List ready to hold the converted nums
+            List<int> listOfNumbers = new List<int>();
+
+            // Then I'll convert it to the int type
+            foreach (string strNum in stringNums)
+            {
+                // The conversion
+                int.TryParse(strNum, out int num);
+                // Adding to the new even list
+                listOfNumbers.Add(num);
+            }
+
+            // Pass it through to the function with some error handling
+            List<int> evens = OnlyReturningEvenNumbers(listOfNumbers);
+
+            // This is a List method to check to see if the Evens List has any elements
+            if (!evens.Any())
+            {
+                Console.WriteLine("Doesn't look like there was any evens!");
+            }
+            else
+            {
+                Console.WriteLine("\nEven ones are: ");
+                foreach (int e in evens)
+                {
+
+                    Console.WriteLine($"{e}");
+                }
+            }
+        }
+
+        private static List<int> OnlyReturningEvenNumbers(List<int> listOfNumbers)         
         {
             List<int> newEvenList = new List<int>();
 
