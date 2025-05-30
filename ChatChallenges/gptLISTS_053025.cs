143c518dbc8eed85e81e81532a9a69a44098c170
mttmortensen <mtt.mortensen527@gmail.com>
2025-05-30 14:42:31 -0600

    starting a new warmup challenge on lists. finding large number now

---
+++ ChatChallenges/gptLISTS_053025.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatChallenges
+{
+    public class gptLISTS_053025
+    {
+        private static int FindingTheLargestNumberInAList(List<int> listOfnums)
+        {
+            int currentMax = 0;
+
+            foreach (int num in listOfnums)
+            {
+                if (num > currentMax) 
+                {
+                    currentMax = num;
+                }
+            }
+
+            return currentMax;
+        }
+    }
+}
