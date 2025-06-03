a2b8d02131051f75ff148f65fa3d4d973f15128b
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-02 21:36:07 -0600

    header for leetcode-like challenge

---
+++ Leetcode/gpt060225_OddList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class gpt060225_OddList
+    {
+
+        public static List<int> RemoveAllOddNumbersFromAList(List<int> targetListOfNums) 
+        {
+            return new List<int>();
+        }
+    }
+}
