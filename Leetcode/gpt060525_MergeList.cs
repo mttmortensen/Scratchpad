8f560e7aa9ff2ae30b3887ee472a9ea774f80d61
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-05 19:06:16 -0600

    basic structure for challenge 2

---
+++ Leetcode/gpt060525_MergeList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class gpt060525_MergeList
+    {
+        public static List<int> MergeTwoListsIntoOneList(List<int> listOne, List<int> listTwo) 
+        {
+            List<int> combinedList = new List<int>();
+
+
+            return combinedList;
+        }
+    }
+}
