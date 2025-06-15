36272a865250aa8bac73f541119be3332d8e527b
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-15 12:02:46 -0600

    start of new challenge on removing duplicates from list

---
+++ Leetcode/gpt061525_DuplicateList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class gpt061525_DuplicateList
+    {
+        public static List<int> RemoveDuplicates(List<int> originalList) 
+        {
+            return new List<int>();
+        }
+    }
+}
