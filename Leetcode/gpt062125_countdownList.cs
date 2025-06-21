1744fa3348a04fe078b6b5139e584c910a77b2c3
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-21 17:40:35 -0600

    next challenge to help better use cases for while loops

---
+++ Leetcode/gpt062125_countdownList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class gpt062125_countdownList
+    {
+        public static void Countdown(int start) { }
+    }
+}
