5e853ce09e0149b959467258d6321600b8428207
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-22 11:45:48 -0600

    method header

--- Leetcode/gpt062225_nestedListss.cs
+++ Leetcode/gpt062225_nestedListss.cs
@@ -8,5 +8,11 @@ namespace Leetcode
 {
     public class gpt062225_nestedListss
     {
+        public static int MaximumWealth(List<List<int>> accounts) 
+        {
+            int totalWealth = 0;
+
+            return totalWealth;
+        }
     }
 }
