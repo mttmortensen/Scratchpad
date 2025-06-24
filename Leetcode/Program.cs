c27b5f5adffe761f5fff219de6afc8a19a792f1d
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-24 16:30:01 -0600

    inital solution for finding most frequent character. will refactor

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,16 +1,18 @@
 ﻿namespace Leetcode
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            List<List<int>> accounts = new List<List<int>> {
-                new List<int> { 1, 2, 3, 6 },   // Total: 12
-                new List<int> { 3, 4 },      // Total: 7
-                new List<int> { 7, 1, 0, 5 }    // Total: 13
-            };
+            string str = Console.ReadLine();
 
-            Console.WriteLine(gpt062225_nestedListss.MaximumWealth(accounts));
+            Dictionary<char, int> results = gpt062225_frequentList.MostFrequetCharacterListed(str);
+
+            foreach(KeyValuePair<char,int> item in results) 
+            {
+                Console.WriteLine($"Highest letter was {item.Key} at {item.Value} ");
+            }
+
 
             Console.ReadKey();
         }
--- Leetcode/gpt062225_frequentList.cs
+++ Leetcode/gpt062225_frequentList.cs
@@ -8,9 +8,40 @@ namespace Leetcode
 {
     public class gpt062225_frequentList
     {
-        public static char MostFrequetCharacterListed(string str) 
+        public static Dictionary<char, int> MostFrequetCharacterListed(string str) 
         {
-            return '\0';
+            char[] chars = str.ToCharArray();
+            int currentCount = 0;
+            int maxCount = 0;
+            Dictionary<char, int> charCounting = new Dictionary<char, int>();
+
+            foreach (char c in chars)
+            {
+                if (charCounting.ContainsKey(c)) 
+                {
+                    charCounting[c]++;
+                    currentCount = charCounting[c];
+                }
+                else 
+                {
+                    charCounting.Add(c, 1);
+                }
+
+
+                if (currentCount > maxCount) 
+                {
+                    maxCount = currentCount;
+                }
+            }
+
+            char highestLetter = charCounting.MaxBy(kvp => kvp.Value).Key;
+            int higestLetterValue = charCounting.MaxBy(kvp => kvp.Value).Value;
+            Dictionary<char, int> highestPair = new Dictionary<char, int>
+            {
+                { highestLetter, higestLetterValue }
+            };
+
+            return highestPair;
         }
     }
 }
