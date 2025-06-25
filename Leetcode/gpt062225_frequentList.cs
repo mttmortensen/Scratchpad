acf5c0f174ba7bb958e1ef25b4f339a72663952a
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-25 16:01:41 -0600

    correct solution with no extra repeating steps

--- Leetcode/gpt062225_frequentList.cs
+++ Leetcode/gpt062225_frequentList.cs
@@ -11,37 +11,30 @@ namespace Leetcode
         public static Dictionary<char, int> MostFrequetCharacterListed(string str) 
         {
             char[] chars = str.ToCharArray();
-            int currentCount = 0;
+            char mostFrequest = '\0';
             int maxCount = 0;
             Dictionary<char, int> charCounting = new Dictionary<char, int>();
 
-            foreach (char c in chars)
+            foreach (char c in str.ToLower()) 
             {
-                if (charCounting.ContainsKey(c)) 
-                {
-                    charCounting[c]++;
-                    currentCount = charCounting[c];
-                }
-                else 
-                {
-                    charCounting.Add(c, 1);
-                }
+                if (!char.IsLetterOrDigit(c))
+                    continue;
 
+                if (!charCounting.ContainsKey(c))
+                    charCounting[c] = 0;
 
-                if (currentCount > maxCount) 
+                // Increment the frequency 
+                charCounting[c]++;
+
+                // Immediately check if this is now the new max
+                if (charCounting[c] > maxCount)
                 {
-                    maxCount = currentCount;
+                    maxCount = charCounting[c];
+                    mostFrequest = c;
                 }
             }
 
-            char highestLetter = charCounting.MaxBy(kvp => kvp.Value).Key;
-            int higestLetterValue = charCounting.MaxBy(kvp => kvp.Value).Value;
-            Dictionary<char, int> highestPair = new Dictionary<char, int>
-            {
-                { highestLetter, higestLetterValue }
-            };
-
-            return highestPair;
+            return new Dictionary<char, int> { { mostFrequest, maxCount } };
         }
     }
 }
