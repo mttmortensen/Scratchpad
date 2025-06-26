83a8767096018bebbe0cb6a16c6d2524b4a3744f
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-26 10:50:12 -0600

    learning LINQ, getting some easy challenges from chat. first challenge and solution first try.

--- ChatChallenges/Program.cs
+++ ChatChallenges/Program.cs
@@ -4,7 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        gptLISTS_062225.ListInputForCounting(); 
+        List<int> nums = new List<int>() { 1, 4, 3, 5, 28, 222, 2431 };
+
+        IEnumerable<int> results = gptLINQ_062625.FindAllEvenNumbers(nums);
+
+        foreach (int num in results) 
+        {
+            Console.WriteLine(num);
+        }
 
         Console.ReadLine();
     }
---
+++ ChatChallenges/gptLINQ_062625.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatChallenges
+{
+    public class gptLINQ_062625
+    {
+        public static IEnumerable<int> FindAllEvenNumbers(List<int> numbers) 
+        {
+            return numbers.Where(n => n % 2 == 0);
+        }
+    }
+}
