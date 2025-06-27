045d425aea6baabb67c240d704fcbb0b16f5e956
mttmortensen <mtt.mortensen527@gmail.com>
2025-06-26 18:52:13 -0600

    starting on second lc challenge  from chat. easy level

---
+++ Leetcode/gpt062625_indexAtList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class gpt062625_indexAtList
+    {
+        public static List<int> SmallerNumbersThanCurrent(List<int> nums) 
+        {
+            return new List<int>() { };
+        }
+    }
+}
