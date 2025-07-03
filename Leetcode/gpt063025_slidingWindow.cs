565117a26add4e50650ee73aedf21bca747d37ab
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-03 13:04:47 -0600

    solution for finding ave with sliding window pattern

--- Leetcode/gpt063025_slidingWindow.cs
+++ Leetcode/gpt063025_slidingWindow.cs
@@ -11,16 +11,27 @@ namespace Leetcode
         public static List<double> FindAverageSizeOfSubSizeArray(List<int> nums, int k) 
         {
             List<double> aveList = new List<double>();
-            double totalToDivide = 0;
+            double windowSum = 0;
 
             // Setup the inital window via k
             for(int i = 0; i < k; i++) 
             {
-                totalToDivide += nums[i];
+                windowSum += nums[i];
                 
             }
-            // This sets up the Average as I only need to do it once
-            aveList.Add(totalToDivide / k);
+            // This sets up the Average as I only need to do it once for the inital
+            aveList.Add(windowSum / k);
+
+            // Now I need to setup the sliding window
+            for (int i = k; i < nums.Count; i++) 
+            {
+                windowSum -= nums[i - k];
+
+                windowSum += nums[i];
+
+                // I still want to get the rest of the averages and put them all in the list
+                aveList.Add(windowSum / k);
+            }
 
             return aveList;
         }
