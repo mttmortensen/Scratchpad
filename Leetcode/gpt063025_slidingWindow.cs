bf72bb6a58aa305cfeee844c4da82fd23b53221c
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-03 12:49:45 -0600

    sliding window challenge with finding the ave of each subarry by k

--- Leetcode/gpt063025_slidingWindow.cs
+++ Leetcode/gpt063025_slidingWindow.cs
@@ -8,5 +8,17 @@ namespace Leetcode
 {
     public class gpt063025_slidingWindow
     {
+        public List<double> FindAverageSizeOfSubSizeArray(List<int> nums, int k) 
+        {
+            List<double> aveList = new List<double>();
+            double total = 0;
+
+            // Setup the inital window via k
+            for(int i = 0; i < k; i++) 
+            {
+                total += nums[i];
+                aveList.Add(total / k);
+            }
+        }
     }
 }
