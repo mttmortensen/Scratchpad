2ebd27cada28d9e08c67ed2473ea34ed6df25220
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-16 21:05:50 -0600

    getting the input now set up getStringAsync

--- HttpClientChallenges/Program.cs
+++ HttpClientChallenges/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter a Id to get a Star Wars Character: ");
+            int charId = Int32.Parse(Console.ReadLine());
+
+            HttpStarWars starClient = new HttpStarWars("https://swapi.dev/api/people/");
+
+            Task<string> results = starClient.GetStringAsync(charId);
+
+            Console.WriteLine($"Here's your Star Wars Character: {results}");
         }
     }
 }
