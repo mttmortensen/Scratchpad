308db429b6bae682a2d886ed7225bb0d96623332
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-16 19:39:36 -0600

    solution got spoiled ahead of time. So just removing this.

--- HttpClientChallenges/Program.cs
+++ HttpClientChallenges/Program.cs
@@ -4,14 +4,7 @@
     {
         static void Main(string[] args)
         {
-            HttpGetPokemon poke = new HttpGetPokemon("https://pokeapi.co/api/v2/pokemon/");
-
-            Task results = poke.Get();
-
-            Console.WriteLine($"{results}");
-
-
-            Console.ReadLine();
+            
         }
     }
 }
