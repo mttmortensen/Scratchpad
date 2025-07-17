44a2c00ea524a1ef1677a9bc90c136aa1cb5b758
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-16 21:36:16 -0600

    comments to help

--- HttpClientChallenges/HttpStarWars.cs
+++ HttpClientChallenges/HttpStarWars.cs
@@ -21,15 +21,20 @@ namespace HttpClientChallenges
 
         public async Task<string> GetStringAsync(int id) 
         {
+            // Will always add it to the end
             var response = await _client.GetAsync(id + "/");
 
+            // This is our error checking for the raw json
             response.EnsureSuccessStatusCode();
 
             string json = await response.Content.ReadAsStringAsync();
 
+            // doing short-hand 'using' syntax here
+            // avoids {}
             using JsonDocument doc = JsonDocument.Parse(json);
             JsonElement root = doc.RootElement;
 
+            // this could be error checked as better practice
             string name = root.GetProperty("name").ToString();
             string height = root.GetProperty("height").ToString();
             string birthYear = root.GetProperty("birth_year").ToString();
