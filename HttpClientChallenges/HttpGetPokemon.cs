a53146e0463d43030025aedc1db0c83723ec8609
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-16 18:50:46 -0600

    my first raw attempt at making an http get request

---
+++ HttpClientChallenges/HttpGetPokemon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace HttpClientChallenges
+{
+    public class HttpGetPokemon
+    {
+        public HttpGetPokemon(string url)
+        {
+            new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+        public void Get() 
+        {
+            using(HttpClient client = new HttpClient()) 
+            {
+                client.GetAsync(client.BaseAddress);
+            }
+        }
+    }
+}
