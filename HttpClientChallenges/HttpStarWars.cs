0e279bf1c8bf7b73738e97d6649804d3b978e663
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-16 19:39:45 -0600

    challenge 1 redo

---
+++ HttpClientChallenges/HttpStarWars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientChallenges
+{
+    public class HttpStarWars
+    {
+        private readonly HttpClient _client;
+
+        public HttpStarWars(string baseUrl)
+        {
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+        }
+
+
+    }
+}
