3bed4e6b9d6632e14c9b85d8584ec0a7206f2742
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-17 18:56:58 -0600

    The model the api is tied to

---
+++ BlazorChallenges/Models/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorChallenges.Models 
+{
+    public class Pokemon
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("base_experience")]
+        public int BaseExperience { get; set; }
+
+        public Sprites? Sprites { get; set; }
+    }
+
+    public class Sprites
+    {
+        [JsonPropertyName("front_default")]
+        public string? Front_Default { get; set; }
+    }
+}
