a023497c3406093ea32d824a4db264c7d1868b96
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-20 10:49:54 -0600

    challenge 2, retry. setup base headers and class obj for reading the response

---
+++ HttpClientChallenges/HttpToDoPost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HttpClientChallenges
+{
+    public class HttpToDoPost
+    {
+        private readonly HttpClient _client;
+
+        public HttpToDoPost(string url)
+        {
+            _client = new HttpClient() 
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+        public async Task<string?> PostToDoItem(string title, string body) 
+        {
+            
+        }
+    }
+
+    public class TodoItem 
+    {
+        public int? Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
--- HttpClientChallenges/Program.cs
+++ HttpClientChallenges/Program.cs
@@ -4,11 +4,6 @@
     {
         static async Task Main(string[] args)
         {
-            HttpBlogPost blog = new HttpBlogPost("https://jsonplaceholder.typicode.com/");
-
-            var content = await blog.PostBlogPost("Some Title", "Some body");
-
-            Console.WriteLine(content.Title, content.Id);
 
             Console.ReadLine();
         }
