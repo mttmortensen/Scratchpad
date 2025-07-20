b4c52c570e8f6d0fcac516797123704c9fe64707
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-20 11:05:25 -0600

    solution for challenge 2 redo completed

--- HttpClientChallenges/HttpToDoPost.cs
+++ HttpClientChallenges/HttpToDoPost.cs
@@ -19,9 +19,38 @@ namespace HttpClientChallenges
             };
         }
 
-        public async Task<string?> PostToDoItem(string title, string body) 
+        public async Task<TodoItem?> PostToDoItem(string title, bool done) 
         {
-            
+            // Build the item for POST
+            TodoItem item = new TodoItem()
+            {
+                Title = title,
+                Completed = done
+            };
+
+            // Serialize the item with json 
+            string json = JsonSerializer.Serialize(item);
+
+            // Wrap it in stringContent for HttpContent to be happy
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // Send the POST await request
+            var response = await _client.PostAsync("posts", content);
+
+            // Ensure a successfull response is made
+            response.EnsureSuccessStatusCode();
+
+            // Read the raw JSON response (await)
+            string responseJson = await response.Content.ReadAsStringAsync();
+
+            // Now deserialize (convert) into TodoItem 
+            TodoItem createdItem = JsonSerializer.Deserialize<TodoItem>(responseJson, new JsonSerializerOptions 
+            {
+                // Important for match JSON keys
+                PropertyNameCaseInsensitive = true 
+            });
+
+            return createdItem;
         }
     }
 
--- HttpClientChallenges/Program.cs
+++ HttpClientChallenges/Program.cs
@@ -5,6 +5,24 @@
         static async Task Main(string[] args)
         {
 
+            Console.WriteLine("Enter your Todo Item title: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Has it been completed? (True/False)");
+            bool done = bool.Parse(Console.ReadLine());
+
+            HttpToDoPost todo = new HttpToDoPost("https://jsonplaceholder.typicode.com/");
+            TodoItem item = await todo.PostToDoItem(title, done);
+
+            if(item.Completed)
+            {
+                Console.WriteLine($"Added! Todo Item: {item.Title}, has been completed");
+            }
+            else 
+            {
+                Console.WriteLine($"Added! Todo Item: {item.Title}, hasn't been completed yet");
+            }
+
             Console.ReadLine();
         }
     }
