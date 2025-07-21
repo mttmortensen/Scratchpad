89b9d7ee95195f9dfe2fa80d870858c3b80ba083
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-21 15:38:43 -0600

    Solution for challenge 1

---
+++ ProgramChallenges/ProgramDay1.cs
@@ -0,0 +1,14 @@
+﻿public static class ProgramDay1 
+{
+    public static void Run(string[] args) 
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello Program.cs Bootcamp");
+
+        app.MapGet("/greet/{name}", (string name) => $"Hello, {name}!");
+
+        app.Run();
+    }
+}
