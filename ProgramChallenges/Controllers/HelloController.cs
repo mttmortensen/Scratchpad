b0a1765cf22e512a2a57b651080129576912afc5
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-21 18:17:57 -0600

    simple controller that day 2 will use

---
+++ ProgramChallenges/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgramChallenges.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("sayhi")]
+        public string SayHi() 
+        {
+            return "Hello from Controller!";
+        }
+    }
+}
