24b1dc9e43361830c270d2ca09d6da5af4851642
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-21 18:19:35 -0600

    comments to help

--- ProgramChallenges/Controllers/HelloController.cs
+++ ProgramChallenges/Controllers/HelloController.cs
@@ -3,9 +3,11 @@
 namespace ProgramChallenges.Controllers
 {
     [ApiController]
+    // "/hello/"
     [Route("[controller]")]
     public class HelloController : ControllerBase
     {
+        // "/hello/sayhi/"
         [HttpGet("sayhi")]
         public string SayHi() 
         {
