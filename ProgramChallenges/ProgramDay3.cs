7b80d891f66bb30aa6e0f81c91282768aa031967
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-22 12:10:33 -0600

    structure for day 3 challenge

--- ProgramChallenges/ProgramDay3.cs
+++ ProgramChallenges/ProgramDay3.cs
@@ -2,5 +2,14 @@
 {
     public static class ProgramDay3
     {
+        public static void Run(string[] args) 
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            // 1. Custom middleware (runs on every request)
+            // 2. App built-in middleware for routing 
+            // 3. Add an endpoint
+        }
     }
 }
