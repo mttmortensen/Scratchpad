2e7bf4ffba11edca4902b543d95202cfde11a3ca
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-24 13:03:47 -0600

    writting models to handle api details. Many endpoints are going to bring different results of data.

---
+++ HttpClientChallenges/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientChallenges
+{
+    public class Pokemon
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Base_Experience { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+
+        public List<PokeAbilities> Abilities { get; set; } 
+    }
+}
