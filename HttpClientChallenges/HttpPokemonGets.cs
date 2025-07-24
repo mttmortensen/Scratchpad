b6660e9a1e24a7ff8b4819823514048104e18536
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-24 09:26:46 -0600

    new crud challenges are still confusing for me. Going to focus on just get for http client using a real api (pokemon)

---
+++ HttpClientChallenges/HttpPokemonGets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientChallenges
+{
+    public class HttpPokemonGets
+    {
+        private readonly HttpClient _client;
+
+        public HttpPokemonGets(string url)
+        {
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+    }
+}
