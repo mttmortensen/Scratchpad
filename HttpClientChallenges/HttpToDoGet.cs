26f0d1711d934369fcd2b0420e677fdef227c5bd
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-24 09:10:22 -0600

    new round of CRUD challenges. Start of challenge 5.

---
+++ HttpClientChallenges/HttpToDoGet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientChallenges
+{
+    public class HttpToDoGet
+    {
+        private readonly HttpClient _client;
+
+        public HttpToDoGet(string url)
+        {
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+        public async Task<List<ToDoItem>> GetIncompletedToDos() 
+        {
+
+        }
+    }
+}
