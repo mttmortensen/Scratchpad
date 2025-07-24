2151c8bd8d8f2d776df3973efc7c2fa3a75112f1
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-24 09:37:46 -0600

    solution for get challenge 1

--- HttpClientChallenges/HttpPokemonGets.cs
+++ HttpClientChallenges/HttpPokemonGets.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HttpClientChallenges
@@ -16,6 +17,24 @@ namespace HttpClientChallenges
             {
                 BaseAddress = new Uri(url)
             };
+        }
+
+        public async Task<string> GetPokemonInfo(string pokeName) 
+        {
+            var response = await _client.GetAsync($"pokemon/{pokeName}");
+
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            using JsonDocument doc = JsonDocument.Parse(json);
+            JsonElement root = doc.RootElement;
+
+            string name = root.GetProperty("name").ToString();
+            string id = root.GetProperty("id").ToString();
+            string base_experience = root.GetProperty("base_experience").ToString();
+
+            return $"Pokemon: {name}, ID: {id}, Base Experience: {base_experience}";
         }
     }
 }
--- HttpClientChallenges/Program.cs
+++ HttpClientChallenges/Program.cs
@@ -4,14 +4,14 @@
     {
         static async Task Main(string[] args)
         {
-            HttpToDoDelete todo = new HttpToDoDelete("https://jsonplaceholder.typicode.com/");
+            HttpPokemonGets poke = new HttpPokemonGets("https://pokeapi.co/api/v2/");
 
-            Console.WriteLine("Enter the ID to delete a ToDo Item: ");
-            int Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the name of a Pokemon to look up: ");
+            string pokemon = Console.ReadLine();
 
-            var results = await todo.DeleteToDoItem(Id);
+            var results = await poke.GetPokemonInfo(pokemon);
 
-            Console.WriteLine($"Item is now deleted: {results}");
+            Console.WriteLine($"{results}");
 
             Console.ReadLine();
         }
