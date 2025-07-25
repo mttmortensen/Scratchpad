5a08ebefca73581a200cd8176c1c155ff9c1c2e1
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-25 10:04:45 -0600

    solution for challenge 7. Includes a person model to go through the linq challenge

--- LINQChallenges/Program.cs
+++ LINQChallenges/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace LINQChallenges
+﻿using LINQChallenges.Models;
+
+namespace LINQChallenges
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Person> people = new List<Person>();
+
+            Person person1 = new Person();
+            person1.Name = "matt";
+            person1.Age = 33;
+            people.Add(person1);
+
+            Person person2 = new Person();
+            person2.Name = "sam";
+            person2.Age = 1;
+            people.Add(person2);
+
+            Person person3 = new Person();
+            person3.Name = "ry";
+            person3.Age = 30;
+            people.Add(person3);
+
+            IEnumerable<string> results = gptLINK7_072525.GetPeopleOver18(people);
+            Console.WriteLine("People over 18: ");
+            foreach(string person in results) 
+            {
+                Console.WriteLine($"{person} is over 18");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ LINQChallenges/gptLINK7_072525.cs
@@ -0,0 +1,20 @@
+﻿using LINQChallenges.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQChallenges
+{
+    public class gptLINK7_072525
+    {
+        public static IEnumerable<string> GetPeopleOver18(IEnumerable<Person> people) 
+        {
+
+            return people
+                .Where(p => p.Age > 18)
+                .Select(p => p.Name.ToUpper());
+        }
+    }
+}
