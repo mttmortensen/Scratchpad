a0a50c869b8a2062075d0a584ef4ea50046ee1f4
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-25 15:17:40 -0600

    3 out of 5 done for these mini challenges around select.

--- LINQChallenges/gptLINQSelect_072525.cs
+++ LINQChallenges/gptLINQSelect_072525.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LINQChallenges.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,19 @@ namespace LINQChallenges
 {
     public class gptLINQSelect_072525
     {
+        public static IEnumerable<int> NumbersGreaterThan10(IEnumerable<int> nums) 
+        {
+            return nums.Where(n => n > 10);
+        }
 
+        public static IEnumerable<string> AllNamesInUpperCase(IEnumerable<string> names) 
+        {
+            return names.Select(n => n.ToUpper());
+        }
+
+        public static IEnumerable<int> GettingPeoplesAge(IEnumerable<Person> people) 
+        {
+            return people.Select(p => p.Age);
+        }
     }
 }
