db1a2a092514bb1127433739284b031bfed88693
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-24 19:32:07 -0600

    breaking out linq challenges to help me really understand

---
+++ LINQChallenges/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQChallenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
