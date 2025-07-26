8aa76fc82e412693e56acbb1d2a51968c9689cda
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-25 19:59:25 -0600

    solutions for 2 and 3

--- LINQChallenges/Program.cs
+++ LINQChallenges/Program.cs
@@ -6,31 +6,46 @@ namespace LINQChallenges
     {
         static void Main(string[] args)
         {
-            List<PersonV2> people = new List<PersonV2>();
+            //List<PersonV2> people = new List<PersonV2>();
 
-            PersonV2 person1 = new PersonV2();
-            person1.FirstName = "matt";
-            person1.LastName = "mortensen";
-            person1.Age = 33;
-            people.Add(person1);
+            //PersonV2 person1 = new PersonV2();
+            //person1.FirstName = "matt";
+            //person1.LastName = "mortensen";
+            //person1.Age = 33;
+            //people.Add(person1);
 
-            PersonV2 person2 = new PersonV2();
-            person2.FirstName = "sam";
-            person2.LastName = "mortensen";
-            person2.Age = 1;
-            people.Add(person2);
+            //PersonV2 person2 = new PersonV2();
+            //person2.FirstName = "sam";
+            //person2.LastName = "mortensen";
+            //person2.Age = 1;
+            //people.Add(person2);
 
-            PersonV2 person3 = new PersonV2();
-            person3.FirstName = "ry";
-            person3.LastName = "mortensen";
-            person3.Age = 30;
-            people.Add(person3);
+            //PersonV2 person3 = new PersonV2();
+            //person3.FirstName = "ry";
+            //person3.LastName = "mortensen";
+            //person3.Age = 30;
+            //people.Add(person3);
 
-            IEnumerable<string> results = gptLINQSelectV2_072525.GetFullNames(people);
-            Console.WriteLine("Full Names: ");
-            foreach(string fullName in results) 
+            //IEnumerable<string> results = gptLINQSelectV2_072525.GetTwoOldest(people);
+            //Console.WriteLine("Two Oldest: ");
+            //foreach(string fullName in results) 
+            //{
+            //    Console.WriteLine($"{fullName}");
+            //}
+
+            List<string> words = new List<string>() 
             {
-                Console.WriteLine($"{fullName}");
+                "apple", 
+                "apricot", 
+                "banana", 
+                "blueberry"
+            };
+
+            IEnumerable<string> results = gptLINQSelectV2_072525.GroupWordsByFirstLetter(words);
+
+            foreach(string word in results) 
+            {
+                Console.WriteLine(word);
             }
 
             Console.ReadLine();
--- LINQChallenges/gptLINQSelectV2_072525.cs
+++ LINQChallenges/gptLINQSelectV2_072525.cs
@@ -13,5 +13,19 @@ namespace LINQChallenges
         {
             return people.Select(p => $"{p.FirstName} {p.LastName}");
         }
+
+        public static IEnumerable<string> GetTwoOldest(IEnumerable<PersonV2> people) 
+        {
+            return people
+                .OrderByDescending(p => p.Age)
+                .Take(2)
+                .Select(p => $"Name: {p.FirstName} {p.LastName}, Age: {p.Age}");
+        }
+
+        public static IEnumerable<string> GroupWordsByFirstLetter(IEnumerable<string> words) 
+        {
+            return words.GroupBy(w => w[0])
+                .Select(group => $"{group.Key}: {string.Join(",", group)}");
+        }
     }
 }
