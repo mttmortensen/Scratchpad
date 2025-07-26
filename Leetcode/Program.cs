3a45df61a4abb5811b6bb6fe62b05820238a87a1
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-26 10:13:26 -0600

    doing a run down of the basics for hash map and then running it

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -4,21 +4,10 @@
     {
         static void Main(string[] args)
         {
-            List<int> values = new List<int>() 
-            {
-                1, 3, 2, 6, -1, 4, 1, 8, 2
-            };
+            gpt072625_hashMap_Basics.Run();
 
-            int k = 5;
 
-            List<double> results = gpt063025_slidingWindow.FindAverageSizeOfSubSizeArray(values, k);
-
-            foreach(double num in results) 
-            {
-                Console.WriteLine($"{num}");
-            }
-
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
--- Leetcode/gpt072625_hashMap_Basics.cs
+++ Leetcode/gpt072625_hashMap_Basics.cs
@@ -11,10 +11,23 @@ namespace Leetcode
         public static void Run() 
         {
             // 1. Create a Dictionary
+            Dictionary<string, int> scores = new Dictionary<string, int>();
+
             // 2. Add Values
+                // We are declaring the 'Alice' Key and assigning it the value of 95 in the same line
+            scores["Alice"] = 95;
+            scores["Bob"] = 87;
+
             // 3. Update Values
+            scores["Alice"] = 99;
+
             // 4. Check if Key exists
+            if (scores.ContainsKey("Bob"))
+                Console.WriteLine($"Bob's score {scores["Bob"]}");
+
             // 5. Iterate over dictionary 
+            foreach(var kvp in scores)
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
         }
     }
 }
