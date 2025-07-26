d481dd8769ad0dd22d38008b78d8e07396fc9f1b
mttmortensen <mtt.mortensen527@gmail.com>
2025-07-26 10:21:08 -0600

    solution for challenge 2 and implamentation

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
-            gpt072625_hashMap_Basics.Run();
+            List<int> nums = [1, 2, 3, 1, 5];
 
+            Dictionary<int, int> results = gpt072625_hashMap_CountOccurrences.CountOccurences(nums);
+
+            foreach(KeyValuePair<int, int> item in results) 
+            {
+                Console.WriteLine($"Number: {item.Key}, Appears: {item.Value}");
+            }
 
             Console.ReadLine();
         }
--- Leetcode/gpt072625_hashMap_CountOccurrences.cs
+++ Leetcode/gpt072625_hashMap_CountOccurrences.cs
@@ -8,5 +8,19 @@ namespace Leetcode
 {
     public class gpt072625_hashMap_CountOccurrences
     {
+        public static Dictionary<int, int>CountOccurrences(List<int> nums) 
+        {
+            Dictionary<int, int> occurrences = new Dictionary<int, int>();
+
+            foreach(int num in nums) 
+            {
+                if (occurrences.ContainsKey(num))
+                    occurrences[num]++;
+                else
+                    occurrences[num] = 1;
+            }
+
+            return occurrences;
+        }
     }
 }
